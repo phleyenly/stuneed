18d154cb68b18f6b5004fe4bece8b8041205eb74
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-04-12 16:40:55 +0700

    header & footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ErrorComponent } from './views/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component: HomeComponent },
+  { path : 'home', component: HomeComponent },
+
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './component/defaul-layout/client/header/header.component';
+import { FooterComponent } from './component/defaul-layout/client/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { ErrorComponent } from './views/error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
